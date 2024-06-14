a46bb6bd64808dc2c1c1283c6e1003c8d25414b1
AntonioFromBrazil <antonio@testa.adv.br>
2024-06-14 10:09:23 -0300

    Update exemplo2.ino

--- examples/exemplo2/exemplo2.ino
+++ examples/exemplo2/exemplo2.ino
@@ -23,7 +23,7 @@ WiFiServer server(80);
 
 String header;
 
-String html="<html><head><meta charset='utf-8'><title></title></head><body><table border='0' width='600' id='table1' cellspacing='4' cellpadding='4' height='400'><tr><td colspan='6' align='center'><p align='center'><b><font size='4'>CONTROLE DO MOTOR DE PASSO<br>PARA TESTE DA PCI_UG_T1 VIA Wi-Fi</font></b></p></td></tr><tr><td colspan='6' align='center'>&nbsp;</td></tr><tr><td colspan='6' align='center' bgcolor='#FFCCFF'><font size='2'><b>VELOCIDADE</b></font></td></tr><tr><td align='center' bgcolor='#CV3' width='100'><a href='/V3'><font size='4'>&nbsp;&nbsp;3&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV5' width='100'><a href='/V5'><font size='4'>&nbsp;&nbsp;5&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV10' width='100'><a href='/V10'><font size='4'>&nbsp;&nbsp;10&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV20' width='100'><a href='/V20'><font size='4'>&nbsp;&nbsp;20&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV40' width='100'><a href='/V40'><font size='4'>&nbsp;&nbsp;40&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV80' width='100'><a href='/V80'><font size='4'>&nbsp;&nbsp;80&nbsp;&nbsp;</font></td></a></tr><tr><td colspan='6' align='center'>&nbsp;</td></tr><tr><td colspan='6' align='center' bgcolor='#CCFF99'><p align='center'><font size='2'><b>DIREÇÃO</b></font></td></tr><tr><td colspan='3' align='center' bgcolor='#CDE' width='300'><a href='/DE'><font size='4'>&nbsp;&nbsp;&nbsp;&nbsp;ESQUERDA&nbsp;&nbsp;&nbsp;&nbsp;</font></td></a><td colspan='3' align='center' bgcolor='#CDD' width='300'><a href='/DD'><font size='4'>&nbsp;&nbsp;&nbsp;&nbsp;DIREITA&nbsp;&nbsp;&nbsp;&nbsp;</font></td></a></tr><tr><td colspan='6' align='center'>&nbsp;</td></tr><tr><td colspan='6' align='center' bgcolor='#99CCFF'><p align='center'><font size='2'><b>MOVIMENTO</b></font></td></tr><tr><td align='center' bgcolor='#C15' width='100'><a href='/C15'><font size='4'>&nbsp;15º&nbsp;</font></td></a><td align='center' bgcolor='#C30' width='100'><a href='/C30'><font size='4'>&nbsp;30º&nbsp;</font></td></a><td align='center' bgcolor='#C45' width='100'><a href='/C45'><font size='4'>&nbsp;45º&nbsp;</font></td></a><td align='center' bgcolor='#C90' width='100'><a href='/C90'><font size='4'>&nbsp;90º&nbsp;</font></td></a><td align='center' bgcolor='#CCP' width='100'><a href='/CCP'><font size='2'>CONT<br>PARA</font></td></a><td align='center' bgcolor='#VV3' width='100'><a href='/VV3'><font size='2'>VOLTAS<br>360º</font></td></a></tr></table></body></html>";
+String html="<html><head><meta charset='utf-8'><title></title></head><body><table border='0' width='600' id='table1' cellspacing='4' cellpadding='4' height='400'><tr><td colspan='6' align='center'><p align='center'><b><font size='4'>CONTROLE DO MOTOR DE PASSO<br>PARA TESTE DA PCI_UG_T1 VIA Wi-Fi</font></b></p></td></tr><tr><td colspan='6' align='center'>&nbsp;</td></tr><tr><td colspan='6' align='center' bgcolor='#FFCCFF'><font size='2'><b>VELOCIDADE</b></font></td></tr><tr><td align='center' bgcolor='#CV3' width='100'><a href='/V3'><font size='4'>&nbsp;&nbsp;3&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV5' width='100'><a href='/V5'><font size='4'>&nbsp;&nbsp;5&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV10' width='100'><a href='/V10'><font size='4'>&nbsp;&nbsp;10&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV20' width='100'><a href='/V20'><font size='4'>&nbsp;&nbsp;20&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV40' width='100'><a href='/V40'><font size='4'>&nbsp;&nbsp;40&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV80' width='100'><a href='/V80'><font size='4'>&nbsp;&nbsp;80&nbsp;&nbsp;</font></td></a></tr><tr><td colspan='6' align='center'>&nbsp;</td></tr><tr><td colspan='6' align='center' bgcolor='#CCFF99'><p align='center'><font size='2'><b>DIREÇÃO</b></font></td></tr><tr><td colspan='3' align='center' bgcolor='#CDE' width='300'><a href='/DE'><font size='4'>&nbsp;&nbsp;&nbsp;&nbsp;ESQUERDA&nbsp;&nbsp;&nbsp;&nbsp;</font></td></a><td colspan='3' align='center' bgcolor='#CDD' width='300'><a href='/DD'><font size='4'>&nbsp;&nbsp;&nbsp;&nbsp;DIREITA&nbsp;&nbsp;&nbsp;&nbsp;</font></td></a></tr><tr><td colspan='6' align='center'>&nbsp;</td></tr><tr><td colspan='6' align='center' bgcolor='#99CCFF'><p align='center'><font size='2'><b>MOVIMENTO</b></font></td></tr><tr><td align='center' bgcolor='#C15' width='100'><a href='/C15'><font size='4'>&nbsp;15º&nbsp;</font></td></a><td align='center' bgcolor='#C30' width='100'><a href='/C30'><font size='4'>&nbsp;30º&nbsp;</font></td></a><td align='center' bgcolor='#C45' width='100'><a href='/C45'><font size='4'>&nbsp;45º&nbsp;</font></td></a><td align='center' bgcolor='#C90' width='100'><a href='/C90'><font size='4'>&nbsp;90º&nbsp;</font></td></a><td align='center' bgcolor='#CCP' width='100'><a href='/CCP'><font size='2'>ANDA<br>PARA</font></td></a><td align='center' bgcolor='#1VS' width='100'><a href='/1VS'><font size='2'>UMA DOSE<br>VIA SENSOR</font></td></a></tr></table></body></html>";
 String copyhtml;
 
 uint32_t nsteps;   //steps a percorrer
@@ -48,10 +48,6 @@ void setup() {
 
 void loop(){
   WiFiClient client = server.available();
-  if ((graus==555)&&(x.xsteps[0]==0)&&(!client)){
-    x.xms=500;while(x.xms>0){}
-    if (sent=='D'){x.runStep(0, 2048, vel, true);}else{x.runStep(0, 2048, vel, false);}
-  }
   if (client) {
     if (((graus==555)||(graus==999))&&(x.xsteps[0]>0)){x.xsteps[0]=2;} //se chegou comando via browser, aborta movimento
     String currentLine = "";
@@ -90,7 +86,7 @@ void loop(){
             if (header.indexOf("GET /C45") >= 0) {graus=45;}
             if (header.indexOf("GET /C90") >= 0) {graus=90;}
             if (header.indexOf("GET /CCP") >= 0) {if (graus==999){graus=0;}else{graus=999;}}
-            if (header.indexOf("GET /VV3") >= 0) {graus=555;}
+            if (header.indexOf("GET /1VS") >= 0) {graus=555;}
 
             copyhtml=html;
 
@@ -114,16 +110,37 @@ void loop(){
             if (graus==45) {copyhtml.replace("#C45", "#0099FF");}else{copyhtml.replace("#C45", "#99CCFF");}
             if (graus==90) {copyhtml.replace("#C90", "#0099FF");}else{copyhtml.replace("#C90", "#99CCFF");}
             if (graus==999){copyhtml.replace("#CCP", "#0099FF");}else{copyhtml.replace("#CCP", "#99CCFF");}
-            if (graus==555){copyhtml.replace("#VV3", "#0099FF");}else{copyhtml.replace("#VV3", "#99CCFF");}
+            if (graus==555){copyhtml.replace("#1VS", "#0099FF");}else{copyhtml.replace("#1VS", "#99CCFF");}
 
             // envia a página com os botões atualizados
             client.println(copyhtml);
 
-            //starta o motor
-            while(x.xsteps[0]>0){}
-            if ((graus==999)&&(graus!=555)){nsteps=2048000;}
-            if ((graus!=999)&&(graus!=555)){nsteps=map(graus,0,360,0,2048);}
-            if (sent=='D'){x.runStep(0, nsteps, vel, true);}else{x.runStep(0, nsteps, vel, false);}
+            //starta o motor de acordo com o comando recebido
+            if (graus!=555){
+              while(x.xsteps[0]>0){}
+              if (graus==999){nsteps=2048000;}
+              if (graus!=999){nsteps=map(graus,0,360,0,2048);}
+              if (sent=='D'){x.runStep(0, nsteps, vel, true);}else{x.runStep(0, nsteps, vel, false);}
+            }  
+
+            //se foi para rodar uma dose baseado só no sensor, executa
+            if (graus==555){
+              Serial.println("");
+              bool dir;
+              if (sent=='D'){dir=true;}else{dir=false;}
+              while (analogRead(34)<3900){
+                Serial.print(analogRead(34));Serial.print(",");
+                while(x.xsteps[0]>0){};
+                x.runStep(0, 6, vel, dir); //gira aprox 1 grau por vez até o início de uma divisão (preto)
+              }  
+              Serial.println("");
+              while(x.xsteps[0]>0){};
+              while (analogRead(34)>3900){
+                Serial.print(analogRead(34));Serial.print(",");
+                while(x.xsteps[0]>0){};
+                x.runStep(0, 6, vel, dir); //gira aprox 1 grau por vez até o fim da divisão (sai do preto)
+              }
+            }
 
             // The HTTP response ends with another blank line
             client.println();
